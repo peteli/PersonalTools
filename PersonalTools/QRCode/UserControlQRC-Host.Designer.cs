6805579bed15c7b76d0254717f111622038746f4
peteli <peter.list@pdpt.rocks>
2019-01-26 21:02:53 +0100

    initiated new function - not finished

---
+++ PersonalTools/QRCode/UserControlQRC-Host.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace peteli.PersonalTools.QRCode
+{
+    partial class UserControlQRC_Host
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.userControlQRC1 = new peteli.PersonalTools.QRCode.UserControlQRC();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(230, 523);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.userControlQRC1;
+            // 
+            // UserControlQRC_Host
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.elementHost1);
+            this.Name = "UserControlQRC_Host";
+            this.Size = new System.Drawing.Size(230, 523);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private UserControlQRC userControlQRC1;
+    }
+}
---
+++ PersonalTools/QRCode/UserControlQRC.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRCoder;
+using System.Drawing;
+using System.Windows.Interop;
+
+namespace peteli.PersonalTools.QRCode
+{
+    /// <summary>
+    /// Interaktionslogik für UserControlQRC.xaml
+    /// </summary>
+    public partial class UserControlQRC : UserControl
+    {
+        public UserControlQRC()
+        {
+            InitializeComponent();
+            this.btnMakeQRCode.Click += BtnMakeQRCode_Click;
+        }
+
+        private void BtnMakeQRCode_Click(object sender, RoutedEventArgs e)
+        {
+            string QRinput = this.QRcontent.Text;
+            if (string.IsNullOrWhiteSpace(QRinput))
+            {
+                QRinput = "Need Input, Dude!";
+             };
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(QRinput, QRCodeGenerator.ECCLevel.Q);
+            QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            this.ImageQRCode.Source = Imaging.CreateBitmapSourceFromHBitmap(qrCodeImage.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()); ;
+        }
+    }
+}
